cf23a034179450a1fb15e5b013bedd55b8895f90
Jake Van Buren <jakevb8@gmail.com>
2026-03-07 14:52:25 -0500

    Add GitHub Actions CI/CD, fix FirebaseApp.configure(), add FirebaseCore dependency

--- NexusCoreIOS/NexusCoreIOSApp.swift
+++ NexusCoreIOS/NexusCoreIOSApp.swift
@@ -1,9 +1,14 @@
 import SwiftUI
+import FirebaseCore
 import FirebaseAuth
 
 @main
 struct NexusCoreIOSApp: App {
     @StateObject private var authState = AuthState()
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
